345afcfe467415a8616c739fb467abd362328541
f5078775 <f5078775@ESCTA091915E405.intrabb.bb.com.br>
2019-04-09 17:48:58 -0300

    atualizado 09/04/2019

--- src/java/br/com/calculopoupanca/controller/ControleGenerico.java
+++ src/java/br/com/calculopoupanca/controller/ControleGenerico.java
@@ -72,18 +72,75 @@ public class ControleGenerico implements Serializable {
                 getComplementoPoupanca().setCorrecaoEsperada(getComplementoPoupanca().getValorBase().multiply(new BigDecimal("0.2235907655")).setScale(2, RoundingMode.HALF_EVEN));
                 getComplementoPoupanca().setFazJus("SIM");
                 getComplementoPoupanca().setObservacao("BRESSER");
+                
+                if(getComplementoPoupanca().getValorBase().compareTo( new BigDecimal("5000"))<= 0){
+                    getComplementoPoupanca().setFaixaDesconto("FAIXA 1");
+                    getComplementoPoupanca().setPercentualDesconto(new BigDecimal("0"));
+                }
+                if(getComplementoPoupanca().getValorBase().compareTo( new BigDecimal("5000"))>0 && getComplementoPoupanca().getValorBase().compareTo( new BigDecimal("10000"))<= 0){
+                    getComplementoPoupanca().setFaixaDesconto("FAIXA 2");
+                    getComplementoPoupanca().setPercentualDesconto(new BigDecimal("0.08"));
+                }
+                if(getComplementoPoupanca().getValorBase().compareTo( new BigDecimal("10000"))>0 && getComplementoPoupanca().getValorBase().compareTo( new BigDecimal("20000"))<= 0){
+                    getComplementoPoupanca().setFaixaDesconto("FAIXA 3");
+                    getComplementoPoupanca().setPercentualDesconto(new BigDecimal("0.14"));
+                }
+                if(getComplementoPoupanca().getValorBase().compareTo( new BigDecimal("20000"))>0 ){
+                    getComplementoPoupanca().setFaixaDesconto("FAIXA 4");
+                    getComplementoPoupanca().setPercentualDesconto(new BigDecimal("0.19"));
+                }
+                
             } else if ((getComplementoPoupanca().getDataBase().before(data4) || getComplementoPoupanca().getDataBase().equals(data4)) && (getComplementoPoupanca().getDataBase().after(data3) || getComplementoPoupanca().getDataBase().equals(data3))) {
                 getComplementoPoupanca().setPlano("VERAO");
                 this.getComplementoPoupanca().setValorAcordo(getComplementoPoupanca().getValorBase().multiply(new BigDecimal("4.09818")).setScale(2, RoundingMode.HALF_EVEN));
                 getComplementoPoupanca().setCorrecaoEsperada(getComplementoPoupanca().getValorBase().multiply(new BigDecimal("0.2235907655")).setScale(2, RoundingMode.HALF_EVEN));
                 getComplementoPoupanca().setFazJus("SIM");
                  getComplementoPoupanca().setObservacao("VERAO");
+                 
+                 if(getComplementoPoupanca().getValorBase().compareTo( new BigDecimal("5000"))<= 0){
+                    getComplementoPoupanca().setFaixaDesconto("FAIXA 1");
+                    getComplementoPoupanca().setPercentualDesconto(new BigDecimal("0"));
+                }
+                if(getComplementoPoupanca().getValorBase().compareTo( new BigDecimal("5000"))>0 && getComplementoPoupanca().getValorBase().compareTo( new BigDecimal("10000"))<= 0){
+                    getComplementoPoupanca().setFaixaDesconto("FAIXA 2");
+                    getComplementoPoupanca().setPercentualDesconto(new BigDecimal("0.08"));
+                }
+                if(getComplementoPoupanca().getValorBase().compareTo( new BigDecimal("10000"))>0 && getComplementoPoupanca().getValorBase().compareTo( new BigDecimal("20000"))<= 0){
+                    getComplementoPoupanca().setFaixaDesconto("FAIXA 3");
+                    getComplementoPoupanca().setPercentualDesconto(new BigDecimal("0.14"));
+                }
+                if(getComplementoPoupanca().getValorBase().compareTo( new BigDecimal("20000"))>0 ){
+                    getComplementoPoupanca().setFaixaDesconto("FAIXA 4");
+                    getComplementoPoupanca().setPercentualDesconto(new BigDecimal("0.19"));
+                }
+                
+                 
+                 
             } else if ((getComplementoPoupanca().getDataBase().before(data6) || getComplementoPoupanca().getDataBase().equals(data6)) && (getComplementoPoupanca().getDataBase().after(data5) || getComplementoPoupanca().getDataBase().equals(data5))) {
                 getComplementoPoupanca().setPlano("COLOR II");
                 this.getComplementoPoupanca().setValorAcordo(getComplementoPoupanca().getValorBase().multiply(new BigDecimal("0.0014")).setScale(2, RoundingMode.HALF_EVEN));
                 getComplementoPoupanca().setCorrecaoEsperada(getComplementoPoupanca().getValorBase().multiply(new BigDecimal("0.2235907655")).setScale(2, RoundingMode.HALF_EVEN));
                 getComplementoPoupanca().setFazJus("SIM");
                  getComplementoPoupanca().setObservacao("COLOR II");
+                  
+                 if(getComplementoPoupanca().getValorBase().compareTo( new BigDecimal("5000"))<= 0){
+                    getComplementoPoupanca().setFaixaDesconto("FAIXA 1");
+                    getComplementoPoupanca().setPercentualDesconto(new BigDecimal("0"));
+                }
+                if(getComplementoPoupanca().getValorBase().compareTo( new BigDecimal("5000"))>0 && getComplementoPoupanca().getValorBase().compareTo( new BigDecimal("10000"))<= 0){
+                    getComplementoPoupanca().setFaixaDesconto("FAIXA 2");
+                    getComplementoPoupanca().setPercentualDesconto(new BigDecimal("0.08"));
+                }
+                if(getComplementoPoupanca().getValorBase().compareTo( new BigDecimal("10000"))>0 && getComplementoPoupanca().getValorBase().compareTo( new BigDecimal("20000"))<= 0){
+                    getComplementoPoupanca().setFaixaDesconto("FAIXA 3");
+                    getComplementoPoupanca().setPercentualDesconto(new BigDecimal("0.14"));
+                }
+                if(getComplementoPoupanca().getValorBase().compareTo( new BigDecimal("20000"))>0 ){
+                    getComplementoPoupanca().setFaixaDesconto("FAIXA 4");
+                    getComplementoPoupanca().setPercentualDesconto(new BigDecimal("0.19"));
+                }
+                
+                 
             } else if ((getComplementoPoupanca().getDataBase().before(data8) || getComplementoPoupanca().getDataBase().equals(data8)) && (getComplementoPoupanca().getDataBase().after(data7) || getComplementoPoupanca().getDataBase().equals(data7))) {
                 getComplementoPoupanca().setPlano("COLOR I");
                 getComplementoPoupanca().setFazJus("NAO");
